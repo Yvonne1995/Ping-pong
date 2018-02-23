a6b4ed52837eab9b338c3c7aaa305f56511c2d7d
Yvonne1995 <ykagondu@gmail.com>
2018-02-23 18:22:58 +0300

    css and js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// user interface
+$("form#inputForm").submit(function(event) {
+  var numbers = parseInt($("input").val());
+  $(".output").empty();
+
+// business logic
+  for (var index = 1; index <= numbers; index += 1) {
+    if (index > 2000) {
+        alert ("try a smaller number!");
+    }
+  else{  if (index % 15 == 0) {
+      $(".output").append("<li>" + "PingPong" + "</li>");
+    } else if (index % 3 == 0) {
+      $(".output").append("<li>" + "Ping" + "</li>");
+    } else if (index % 5 == 0) {
+      $(".output").append("<li>" + "Pong" + "</li>");
+    } else {
+      $(".output").append("<li>" + index + "</li>");
+    }
+  }}
+  event.preventDefault();
+});
