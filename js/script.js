54002f69f511b0ef8b5b5d5394dbbaafe8e34910
Yvonne1995 <ykagondu@gmail.com>
2018-02-26 10:54:33 +0300

    js changes

--- js/script.js
+++ js/script.js
@@ -3,20 +3,22 @@ $("form#inputForm").submit(function(event) {
   var numbers = parseInt($("input").val());
   $(".output").empty();
 
-// business logic
-  for (var index = 1; index <= numbers; index += 1) {
-    if (index > 2000) {
-        alert ("try a smaller number!");
+  // business logic
+  if (numbers > 2000) {
+    alert("try a smaller number!");
+  } else {
+    for (var index = 1; index <= numbers; index += 1) {
+
+      if (index % 15 == 0) {
+        $(".output").append("<li>" + "PingPong" + "</li>");
+      } else if (index % 3 == 0) {
+        $(".output").append("<li>" + "Ping" + "</li>");
+      } else if (index % 5 == 0) {
+        $(".output").append("<li>" + "Pong" + "</li>");
+      } else {
+        $(".output").append("<li>" + index + "</li>");
+      }
     }
-  else{  if (index % 15 == 0) {
-      $(".output").append("<li>" + "PingPong" + "</li>");
-    } else if (index % 3 == 0) {
-      $(".output").append("<li>" + "Ping" + "</li>");
-    } else if (index % 5 == 0) {
-      $(".output").append("<li>" + "Pong" + "</li>");
-    } else {
-      $(".output").append("<li>" + index + "</li>");
-    }
-  }}
+  }
   event.preventDefault();
 });
